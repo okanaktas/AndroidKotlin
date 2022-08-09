4a990ab0c8860e23067ea230c69e16fb82859ea3
Okan Aktaş <okanaktas02@gmail.com>
2022-08-09 05:07:08 +0300

    Binding

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -2,15 +2,19 @@ package com.okanaktas.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.okanaktas.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val person = Person("okan","aktas",26)
 
-        println(person.age)
 
     }
 }
--- MyApplication/app/src/main/java/com/okanaktas/myapplication/Person.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/Person.kt
@@ -1,16 +1,4 @@
 package com.okanaktas.myapplication
 
 class Person (var name: String,var surName: String,var age : Int){
-
-    /*
-    var name = ""
-    var surName =""
-    var age = 0
-
-    constructor(name : String, surName :String, age : Int){
-        this.name = name
-        this.surName = surName
-        this.age = age
-    }
-     */
 }
