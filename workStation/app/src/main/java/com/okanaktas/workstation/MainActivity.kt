f3616f5499d451e0822d06f01258677cd79a3a91
Okan Aktaş <okanaktas02@gmail.com>
2022-08-24 19:21:43 +0300

    Trial Example

--- workStation/app/src/main/java/com/okanaktas/workstation/MainActivity.kt
+++ workStation/app/src/main/java/com/okanaktas/workstation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.okanaktas.workstation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.okanaktas.workstation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +15,15 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
     }
+    
+    fun buttonLogin(view: View){
+        if( binding.nameText.text.equals("okan") && binding.passwordText.text.equals("123123")){
+            Toast.makeText(applicationContext,"True infromation!",Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(applicationContext,"Wrong infromation",Toast.LENGTH_LONG).show()
+        }
 
-    fun buttonActive(view : View){
-        binding.textView.setText("Button Actived")
     }
+
 }
