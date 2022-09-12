94b35475421fdbc37824b5a55b13e40097c082fe
Okan Aktaş <okanaktas02@gmail.com>
2022-09-13 01:54:07 +0300

    update

---
+++ CatchCartoonCharacters/app/src/main/java/com/okanaktas/catchcartooncharacters/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.okanaktas.catchcartooncharacters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.okanaktas.catchcartooncharacters.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+}
