c7be12a94f26bccfabbbbb7522df6b3ba71474eb
Okan Aktaş <okanaktas02@gmail.com>
2022-09-12 22:34:19 +0300

    Kotlin Android

--- Begin/app/src/main/java/com/okanaktas/begin/MainActivity.kt
+++ Begin/app/src/main/java/com/okanaktas/begin/MainActivity.kt
@@ -2,15 +2,17 @@ package com.okanaktas.begin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.okanaktas.begin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    var runnable : Runnable = Runnable{}
+    var handler : Handler = Handler ()
+
     var number = 0
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,17 +20,26 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+
+    fun buttonStart(view : View){
+
+        runnable = object : Runnable {
+            override fun run() {
+                binding.textViewTimer.setText("Timer: $number")
+                number++
+
+                handler.postDelayed(runnable, 1000)
+            }
+        }
+        handler.post(runnable)
 
 
     }
 
-    fun buttonStart(view: View){
-
-        binding.textViewTimer.setText("Timer: ${number}")
-        number++
-    }
     fun buttonStop(view : View){
-
+        handler.removeCallbacks(runnable)
+        binding.textViewTimer.setText("Timer: ")
+        //number = 0
     }
-
 }
