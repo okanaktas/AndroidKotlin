b8f9e468300768fa1a69c58b400dd1662d50fec7
Okan Aktaş <okanaktas02@gmail.com>
2022-09-13 03:34:46 +0300

    add
    add runnable and handler
    add text and image

--- CatchCartoonCharacters/app/src/main/java/com/okanaktas/catchcartooncharacters/GameActivity.kt
+++ CatchCartoonCharacters/app/src/main/java/com/okanaktas/catchcartooncharacters/GameActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.view.View
 import com.okanaktas.catchcartooncharacters.databinding.ActivityGameBinding
 import com.okanaktas.catchcartooncharacters.databinding.ActivityMainBinding
 
@@ -12,6 +13,8 @@ class GameActivity : AppCompatActivity() {
 
     var runnable : Runnable = Runnable {}
     var handler : Handler = Handler()
+
+    var score : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +30,13 @@ class GameActivity : AppCompatActivity() {
 
         //Gelen ismi alma
         var inputName = intent.getStringExtra("name")
-        binding.textViewPlayerName.setText("Player Name: $inputName")
+        binding.textViewPlayerName.setText("Player: $inputName")
 
         //Runnable oluşturma
         runnable = object : Runnable{
             override fun run() {
                 newDeger--
-                binding.textViewInputTime.setText("Remaining Seconds: $newDeger")
+                binding.textViewInputTime.setText("Seconds: $newDeger")
 
                 handler.postDelayed(runnable,1000)
             }
@@ -42,5 +45,17 @@ class GameActivity : AppCompatActivity() {
 
 
 
+    }
+
+    fun increaseScore(view : View){
+        runnable = object : Runnable{
+            override fun run() {
+                score++
+                binding.textViewScore.setText("Score: $score")
+
+                handler.postDelayed(runnable,1000)
+            }
+        }
+        handler.post(runnable)
     }
 }
