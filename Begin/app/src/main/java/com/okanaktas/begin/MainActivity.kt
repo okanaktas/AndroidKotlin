4241efa9900ff08f7cc768bf9534a5be0c89241a
Okan Aktaş <okanaktas02@gmail.com>
2022-09-22 02:13:42 +0300

    update

--- Begin/app/src/main/java/com/okanaktas/begin/MainActivity.kt
+++ Begin/app/src/main/java/com/okanaktas/begin/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.okanaktas.begin
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
 import com.okanaktas.begin.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-
+    val imageArray = ArrayList<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +21,40 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        imageArray.add(binding.imageViewB10)
+        imageArray.add(binding.imageViewB2)
+        imageArray.add(binding.imageViewB3)
+        imageArray.add(binding.imageViewB4)
+        imageArray.add(binding.imageViewB5)
+        imageArray.add(binding.imageViewB6)
+        imageArray.add(binding.imageViewB7)
+        imageArray.add(binding.imageViewB8)
+        imageArray.add(binding.imageViewB9)
+        imageArray.add(binding.imageViewBA)
+        imageArray.add(binding.imageViewBJ)
+        imageArray.add(binding.imageViewBK)
+        imageArray.add(binding.imageViewBQ)
 
-        
     }
+
+    fun buttonChoose(view: View) {
+
+        hideImages()
+
+    }
+
+    fun hideImages() {
+
+        binding.imageViewGameImage.visibility = View.INVISIBLE
+        
+        for (i in imageArray) {
+            i.visibility = View.INVISIBLE
+        }
+
+        val random = Random()
+        val randomNumber = random.nextInt(12)
+        imageArray[randomNumber].visibility = View.VISIBLE
+
+    }
+
 }
