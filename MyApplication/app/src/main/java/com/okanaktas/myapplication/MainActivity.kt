1763ee661adde00b4d5e348f7986645306c89e9b
Okan Aktaş <okanaktas02@gmail.com>
2022-10-07 02:54:01 +0300

    practice

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -9,22 +9,12 @@ import com.okanaktas.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    var number : Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-number?.let{
-    println(it *10)
-}
-
-        println(number!!)
-
-
-
 
     }
 }
