5163fcd7087dec91d245db9f4d3ff460b67e0db5
Okan Aktaş <okanaktas02@gmail.com>
2022-10-08 16:50:59 +0300

    database practice

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -16,5 +16,48 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+
+        try {
+
+            val database = this.openOrCreateDatabase("Home", MODE_PRIVATE, null)
+            database.execSQL("CREATE TABLE IF NOT EXISTS birol(name VARCHAR,number INT)")
+            database.execSQL("INSERT INTO birol (name, number) VALUES('aybuke',22)")
+
+            val cursor = database.rawQuery("SELECT * FROM birol",null)
+
+            val index1 = cursor.getColumnIndex("name")
+            var index2 = cursor.getColumnIndex("number")
+
+            while (cursor.moveToNext()){
+                println("Gelen isim degeri: " + cursor.getString(index1))
+                println("Gelen Sayi  degeri: " + cursor.getString(index2))
+            }
+            cursor.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
+        /*try {
+            val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null)
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
+            myDatabase.execSQL("INSERT INTO musicians (name , age) VALUES ('Okan' ,26)")
+
+            val cursor = myDatabase.rawQuery("SELECT * FROM musicians", null)
+
+            val nameIx = cursor.getColumnIndex("name")
+            val ageIx = cursor.getColumnIndex("age")
+
+            while(cursor.moveToNext()){
+                println("Gelen Name Degeri: " + cursor.getString(nameIx))
+                println("Gelen Age Degeri: " + cursor.getInt(ageIx))
+            }
+            cursor.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+*/
     }
 }
