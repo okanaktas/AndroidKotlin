a2ce3a75f97d366c330648df172fe868f267fbff
Okan Aktaş <okanaktas02@gmail.com>
2022-10-19 10:33:09 +0300

    practice

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -16,7 +16,31 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        try {
 
+            val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE,null)
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS person (name VARCHAR, age INT)")
+            myDatabase.execSQL("INSERT INTO person (name, age) VALUES ('aybuke',22)")
+
+            var cursor = myDatabase.rawQuery("SELECT * FROM person",null)
+
+            val nameIX = cursor.getColumnIndex("name")
+            var ageIX = cursor.getColumnIndex("age")
+
+            while(cursor.moveToNext()){
+                println("Birinci deger: " + cursor.getString(nameIX))
+                println("Ikinci deger: " + cursor.getInt(ageIX))
+            }
+            cursor.close()
+
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
+        /*
         try {
 
             val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE, null)
@@ -38,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             e.printStackTrace()
         }
-
+*/
         /*try {
             val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null)
             myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
