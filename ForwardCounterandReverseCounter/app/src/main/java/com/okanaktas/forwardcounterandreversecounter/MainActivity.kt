327d1cb7e449aa101ff302d65b655eca1d3ac2cb
Okan Aktaş <okanaktas02@gmail.com>
2022-11-13 01:05:48 +0200

    update

--- ForwardCounterandReverseCounter/app/src/main/java/com/okanaktas/forwardcounterandreversecounter/MainActivity.kt
+++ ForwardCounterandReverseCounter/app/src/main/java/com/okanaktas/forwardcounterandreversecounter/MainActivity.kt
@@ -2,11 +2,17 @@ package com.okanaktas.forwardcounterandreversecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.okanaktas.forwardcounterandreversecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    val runnable : Runnable = Runnable{}
+    val handler : Handler = Handler()
+
+    var counter = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
     }
     fun buttonForward(view : View){
-
+        runnable : object = 
+        binding.textViewForward.setText("Forward Counter: $counter")
+        counter++
     }
     fun buttonReverse(view : View){
 
