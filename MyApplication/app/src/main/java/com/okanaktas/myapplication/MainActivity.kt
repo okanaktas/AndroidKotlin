8070f5ebabab293656659e9ba9b9abc400a32186
Okan Aktaş <okanaktas02@gmail.com>
2022-11-16 15:11:03 +0200

    Practice

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -9,10 +9,10 @@ import com.okanaktas.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    var runnable: Runnable = Runnable {}
+    var runabble: Runnable = Runnable {}
     var handler: Handler = Handler()
 
-    var timer = 0
+    var input = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,96 +44,94 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             e.printStackTrace()
         }
-
-        /*
-        try {
-
-            val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE, null)
-
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS person (name VARCHAR, age INT)")
-            myDatabase.execSQL("INSERT INTO person (name, age) VALUES ('aybuke',22)")
-
-            var cursor = myDatabase.rawQuery("SELECT * FROM person", null)
-
-            val nameIX = cursor.getColumnIndex("name")
-            var ageIX = cursor.getColumnIndex("age")
-
-            while (cursor.moveToNext()) {
-                println("Birinci deger: " + cursor.getString(nameIX))
-                println("Ikinci deger: " + cursor.getInt(ageIX))
-            }
-            cursor.close()
-
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-*/
-
-        /*
-        try {
-
-            val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE, null)
-
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS class(name VARCHAR, age INT)")
-            myDatabase.execSQL("INSERT INTO class(name, age) VALUES ('okan',26)")
-
-            val cursor = myDatabase.rawQuery("SELECT * FROM class", null)
-
-            val nameIX = cursor.getColumnIndex("name")
-            val ageIX = cursor.getColumnIndex("age")
-
-            while (cursor.moveToNext()) {
-                println("Values of first: " + cursor.getString(nameIX))
-                println("Values of second: " + cursor.getInt(ageIX))
-            }
-            cursor.close()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-*/
-        /*try {
-            val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null)
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
-            myDatabase.execSQL("INSERT INTO musicians (name , age) VALUES ('Okan' ,26)")
-
-            val cursor = myDatabase.rawQuery("SELECT * FROM musicians", null)
-
-            val nameIx = cursor.getColumnIndex("name")
-            val ageIx = cursor.getColumnIndex("age")
-
-            while(cursor.moveToNext()){
-                println("Gelen Name Degeri: " + cursor.getString(nameIx))
-                println("Gelen Age Degeri: " + cursor.getInt(ageIx))
-            }
-            cursor.close()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-*/
     }
 
-    fun buttonClick(view: View) {
+    fun buttonCounter(view: View) {
+        input = binding.editTextNumber.text.toString().toInt()
 
-        runnable = object : Runnable {
+        runabble = object : Runnable {
             override fun run() {
-                binding.textViewTimer.setText("Timer: ${timer}")
-                handler.postDelayed(runnable, 1000)
-                timer++
+                binding.textViewCounter.setText("Counter: $input")
+                input--
+                handler.postDelayed(runabble, 1000)
             }
         }
-        handler.post(runnable)
-        binding.buttonClick.isEnabled = false
-
+        binding.buttonCounter.isEnabled = false
+        handler.post(runabble)
     }
-//buttonStop
-    fun buttonStop(view: View) {
-        handler.removeCallbacks(runnable)
-        timer = 0
-        binding.textViewTimer.setText("Timer: $timer")
-        binding.buttonClick.isEnabled = true
-
+    fun buttonStop(view: View){
+        handler.removeCallbacks(runabble)
+        input = 0
+        binding.textViewCounter.setText("Counter: ")
+        binding.buttonCounter.isEnabled = true
     }
+
+    /*
+    try {
+
+        val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE, null)
+
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS person (name VARCHAR, age INT)")
+        myDatabase.execSQL("INSERT INTO person (name, age) VALUES ('aybuke',22)")
+
+        var cursor = myDatabase.rawQuery("SELECT * FROM person", null)
+
+        val nameIX = cursor.getColumnIndex("name")
+        var ageIX = cursor.getColumnIndex("age")
+
+        while (cursor.moveToNext()) {
+            println("Birinci deger: " + cursor.getString(nameIX))
+            println("Ikinci deger: " + cursor.getInt(ageIX))
+        }
+        cursor.close()
+
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+*/
+
+    /*
+    try {
+
+        val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE, null)
+
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS class(name VARCHAR, age INT)")
+        myDatabase.execSQL("INSERT INTO class(name, age) VALUES ('okan',26)")
+
+        val cursor = myDatabase.rawQuery("SELECT * FROM class", null)
+
+        val nameIX = cursor.getColumnIndex("name")
+        val ageIX = cursor.getColumnIndex("age")
+
+        while (cursor.moveToNext()) {
+            println("Values of first: " + cursor.getString(nameIX))
+            println("Values of second: " + cursor.getInt(ageIX))
+        }
+        cursor.close()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+*/
+    /*try {
+        val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null)
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
+        myDatabase.execSQL("INSERT INTO musicians (name , age) VALUES ('Okan' ,26)")
+
+        val cursor = myDatabase.rawQuery("SELECT * FROM musicians", null)
+
+        val nameIx = cursor.getColumnIndex("name")
+        val ageIx = cursor.getColumnIndex("age")
+
+        while(cursor.moveToNext()){
+            println("Gelen Name Degeri: " + cursor.getString(nameIx))
+            println("Gelen Age Degeri: " + cursor.getInt(ageIx))
+        }
+        cursor.close()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+*/
 }
