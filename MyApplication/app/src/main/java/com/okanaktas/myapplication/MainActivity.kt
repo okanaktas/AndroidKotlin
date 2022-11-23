05ee184ad0ed3a94912110b9a59e7c2297ceb68d
Okan Aktaş <okanaktas02@gmail.com>
2022-11-23 04:31:09 +0200

    practice database

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -9,16 +9,31 @@ import com.okanaktas.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    var runnable: Runnable = Runnable {}
-    var handler: Handler = Handler()
-
-    var counter = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        try {
+            val myDatabase = this.openOrCreateDatabase("School", MODE_PRIVATE,null)
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS student (name VARCHAR, age INT)")
+            myDatabase.execSQL("INSERT INTO person (name,age) VALUES ('okan',26)")
+
+            var cursor = myDatabase.rawQuery("SELECT * FROM person",null)
+
+            val nameIX = cursor.getColumnIndex("name")
+            var ageIX = cursor.getColumnIndex("age")
+
+            while(cursor.moveToNext()){
+                println("Birinci deger: " + cursor.getString(nameIX))
+                println("Ikinci deger: " + cursor.getString(ageIX))
+            }
+            cursor.close()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
 
 
         /*
@@ -46,29 +61,5 @@ class MainActivity : AppCompatActivity() {
     }
 */
 
-    }
-
-    fun buttonStart(view: View) {
-
-        counter = binding.editTextNumber.text.toString().toInt()
-
-        runnable = object : Runnable {
-            override fun run() {
-
-                binding.textViewCounter.setText("Counter: $counter")
-                counter++
-                handler.postDelayed(runnable, 1000)
-            }
-        }
-        handler.post(runnable)
-        binding.buttonStart.isEnabled = false
-
-    }
-
-    fun buttonStop(view: View) {
-        handler.removeCallbacks(runnable)
-        counter = 0
-        binding.buttonStart.isEnabled = true
-        binding.textViewCounter.setText("Counter: ")
     }
 }
