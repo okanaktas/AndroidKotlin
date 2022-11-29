6a7c08141669ffb59c979bf4553b5316e158cb4d
Okan Aktaş <okanaktas02@gmail.com>
2022-11-29 20:55:42 +0200

    new app ArtBook

---
+++ ArtBook/app/src/main/java/com/okanaktas/artbook/ArtActivity.kt
@@ -0,0 +1,18 @@
+package com.okanaktas.artbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.okanaktas.artbook.databinding.ActivityArtBinding
+import com.okanaktas.artbook.databinding.ActivityMainBinding
+
+class ArtActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityArtBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArtBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
