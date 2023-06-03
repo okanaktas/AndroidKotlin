7849f2d916f3201fccc1209ddaccb91054f9abdf
Okan Aktaş <okanaktas02@gmail.com>
2023-06-03 23:29:23 +0300

    Classic Constructor

--- Classes/app/src/main/java/com/okanaktas/classes/MainActivity.kt
+++ Classes/app/src/main/java/com/okanaktas/classes/MainActivity.kt
@@ -1,21 +1,17 @@
-package com.okanaktas.function
+package com.okanaktas.classes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mySum(4,5)
-        myMultipy(2,6 )
-    }
-
-    fun mySum(a: Int, b: Int){
-        print(a+b)
-    }
-    fun myMultipy(a: Int, b:Int) : Int {
-        return a*b
+        val person = Person("okan", 26, "Developer")
+        print(person.name)
     }
 }
---
+++ Classes/app/src/main/java/com/okanaktas/classes/Person.kt
@@ -0,0 +1,14 @@
+package com.okanaktas.classes
+
+class Person {
+
+    var name = ""
+    var age = 0
+    var job = ""
+
+    constructor(name : String, age: Int, job : String){
+        this.name = name
+        this.age = age
+        this.job = job
+    }
+}
