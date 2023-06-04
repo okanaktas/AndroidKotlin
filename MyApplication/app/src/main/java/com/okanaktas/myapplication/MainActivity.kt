90d0837fa4cf7a316fa777ef93afe13c72a48d04
Okan Aktaş <okanaktas02@gmail.com>
2023-06-04 21:30:46 +0300

    Create MyApplication

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -2,44 +2,12 @@ package com.okanaktas.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
-import com.okanaktas.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-var value
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-//public place
-        try {
-            val database = this.openOrCreateDatabase("Office", MODE_PRIVATE, null)
+        setContentView(R.layout.activity_main)
 
-            database.execSQL("CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY, name VARCHAR ,age INT)")
-            database.execSQL("INSERT INTO person (name, age)VALUES('okan',27)")
-
-            var cursor = database.rawQuery("SELECT * FROM person", null)
-
-            var nameIX = cursor.getColumnIndex("name")
-            var ageIX = cursor.getColumnIndex("age")
-
-            //imleç cursor
-            while () {
-                print("First value: " + cursor.getString(nameIX))
-                print("Second value: " + cursor.getString(ageIX))
-            }
-            cursor.close()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
+        print("hello world!")
     }
- //2 button
-    fun buttonStart(view : View){}
-    fun buttonStop(view : View){}
 }
