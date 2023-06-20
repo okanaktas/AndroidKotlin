d207c5ab166244d64163e5f074c4ee92e2eecbad
Okan Aktaş <okanaktas02@gmail.com>
2023-06-20 20:55:42 +0300

    Two Runnable for counter

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -2,10 +2,40 @@ package com.okanaktas.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.okanaktas.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    var runnable: Runnable = Runnable {}
+    var runnableCounter: Runnable = Runnable {}
+    var handler: Handler = Handler()
+
+    var deger: Int = 0
+    var counter: Int = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        runnableCounter = object : Runnable {
+            override fun run() {
+                handler.postDelayed(runnableCounter, 1000)
+                counter-=100
+            }
+        }
+        handler.post(runnableCounter)
+
+        runnable = object : Runnable {
+            override fun run() {
+                handler.postDelayed(runnable, counter.toLong())
+                deger++
+                binding.textView.setText("Number: $deger")
+            }
+        }
+        handler.post(runnable)
     }
 }
