ba3553ae582a4921749afd3c3c090de41b067464
Okan Aktaş <okanaktas02@gmail.com>
2023-06-20 20:03:28 +0300

    create the pratice

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -1,33 +1,11 @@
 package com.okanaktas.myapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.okanaktas.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
-    var sayi = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        sayi = 5
-    }
-
-    fun buttonGo(view: View) {
-
-        val intent = Intent(this, YedekActivity::class.java)
-
-        intent.putExtra("deger",sayi.toString())
-
-
-        startActivity(intent)
-
+        setContentView(R.layout.activity_main)
     }
 }
