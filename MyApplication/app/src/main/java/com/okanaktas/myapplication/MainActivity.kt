d4a4d960008116968ecddc6d56a754784d1167cd
Okan Aktaş <okanaktas02@gmail.com>
2023-07-02 23:59:19 +0300

    update

--- MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -8,36 +8,14 @@ import com.okanaktas.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    var runnable: Runnable = Runnable {}
-    var runnableCounter: Runnable = Runnable {}
-    var handler: Handler = Handler()
-
-    var deger: Int = 0
-    var counter: Int = 1000
+    private lateinit var deger : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
-        //Saniye hızı
-        runnableCounter = object : Runnable {
-            override fun run() {
-                handler.postDelayed(runnableCounter, 1000)
-                counter-=100
-            }
-        }
-        handler.post(runnableCounter)
 
-        //Normal sayı sayma
-        runnable = object : Runnable {
-            override fun run() {
-                handler.postDelayed(runnable, counter.toLong())
-                deger++
-                binding.textView.setText("Number: $deger")
-            }
-        }
-        handler.post(runnable)
+        deger = "Okan"
+
     }
 }
