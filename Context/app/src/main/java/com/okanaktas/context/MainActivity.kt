bade61373430faa83b632b59735f09284e82fafa
Okan Aktaş <okanaktas02@gmail.com>
2023-07-09 04:15:10 +0300

    Context, AlertDialog, Toast

--- Context/app/src/main/java/com/okanaktas/context/MainActivity.kt
+++ Context/app/src/main/java/com/okanaktas/context/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.okanaktas.context
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.okanaktas.context.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +17,29 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // 1)Toast 2)Alert Dialog 3)SnackBar
+
+        //Context
+        //Activity Context --> this
+        //Application Context --> applicationContext
+
+        Toast.makeText(this@MainActivity, "Welcome", Toast.LENGTH_LONG).show()
 
     }
-    fun buttonSave(view : View){
-        
+
+    fun buttonSave(view: View) {
+        //AlertDialog
+        var alertDialog = AlertDialog.Builder(this@MainActivity)
+        alertDialog.setTitle("Header")
+        alertDialog.setMessage("Message")
+        alertDialog.setPositiveButton("Yes!", object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                Toast.makeText(this@MainActivity, "Saved!", Toast.LENGTH_LONG).show()
+            }
+        })
+        alertDialog.setNegativeButton("No!") { p0, p1 ->
+            Toast.makeText(this@MainActivity,"Not Saved!",Toast.LENGTH_LONG).show()
+        }
+        alertDialog.show()
     }
 }
