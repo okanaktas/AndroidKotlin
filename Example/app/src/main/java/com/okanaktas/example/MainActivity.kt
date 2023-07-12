dfeaa58e9137136a19f09c1cf5b8076e07170570
Okan Aktaş <okanaktas02@gmail.com>
2023-07-12 22:32:19 +0300

    intent and sharedPreferences

--- Example/app/src/main/java/com/okanaktas/example/MainActivity.kt
+++ Example/app/src/main/java/com/okanaktas/example/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.okanaktas.example
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,15 +10,22 @@ import com.okanaktas.example.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    lateinit var sharedPref : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        sharedPref = this.getSharedPreferences("com.okanaktas.example", MODE_PRIVATE)
+
     }
     fun buttonSend(view : View){
         var intent = Intent(this@MainActivity,MainActivity2::class.java)
-        intent.putExtra("deger",binding.editText.text.toString())
+       // intent.putExtra("deger",binding.editText.text.toString())
         startActivity(intent)
+
+        sharedPref.edit().putString("deger", binding.editText.text.toString()).apply()
     }
 }
