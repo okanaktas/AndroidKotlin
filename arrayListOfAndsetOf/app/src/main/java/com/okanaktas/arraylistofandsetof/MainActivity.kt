7d619dd17c0a9dd139f521bd054e69813b620691
Okan Aktaş <okanaktas02@gmail.com>
2023-10-04 11:55:26 +0300

    arrayListOf and setOf and hashMapOf

--- arrayListOfAndsetOf/app/src/main/java/com/okanaktas/arraylistofandsetof/MainActivity.kt
+++ arrayListOfAndsetOf/app/src/main/java/com/okanaktas/arraylistofandsetof/MainActivity.kt
@@ -8,14 +8,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+       // <------ arrayList ----->
         var arrayList = arrayListOf<Any>("Okan",27)
         println("First element: ${arrayList[0]}")
 
+        //<------ setOf -------->
         var setList = setOf<Any>(1,1,2,3)
 
         print(setList.size)
 
         //setList içerisindekileri lambda ile foreach kullanarak ekrana bastırma
         setList.forEach { println(it) }
+
+        // <---- map ------->
+        //key - value   
+
+        var fruitList = hashMapOf<String,Int>()
+        fruitList.put("banana",100)
+        fruitList.put("apple",150)
+
+        print(fruitList["apple"])
     }
 }
