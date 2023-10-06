c8c52e489ea787f5cd19f9a433333d89d6a78f82
Okan Aktaş <okanaktas02@gmail.com>
2023-10-06 18:39:28 +0300

    Practice

---
+++ MyProject/app/src/main/java/com/okanaktas/myapplication/Aktas.kt
@@ -0,0 +1,5 @@
+package com.okanaktas.myapplication
+
+class Aktas (var name : String, var age : Int, var job : String) {
+
+}
--- MyProject/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
+++ MyProject/app/src/main/java/com/okanaktas/myapplication/MainActivity.kt
@@ -2,14 +2,35 @@ package com.okanaktas.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var name: EditText
+    lateinit var age: EditText
+    lateinit var job: EditText
+    lateinit var button: Button
+    lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = arrayListOf<Any>(1,1,2,3)
 
-        print("My list first element: $list[0]")
+        name = findViewById(R.id.editTextText)
+        age = findViewById(R.id.editTextText2)
+        job = findViewById(R.id.editTextText3)
+        button = findViewById(R.id.button)
+
+        var okan = Aktas("Okan", 27, "Engineer")
+
+        button.setOnClickListener {
+            text.setText("İsminiz: ${okan.name}, Yaşınız: ${okan.age}, Mesleğiniz: ${okan.job}")
+        }
+
+
     }
 }
