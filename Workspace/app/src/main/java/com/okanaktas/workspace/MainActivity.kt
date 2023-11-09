e249b6e36ccd2217997cad2ad61b5fa7bf609fd2
Okan Aktaş <okanaktas02@gmail.com>
2023-11-09 23:12:41 +0300

    Work Space

--- Workspace/app/src/main/java/com/okanaktas/workspace/MainActivity.kt
+++ Workspace/app/src/main/java/com/okanaktas/workspace/MainActivity.kt
@@ -16,39 +16,7 @@ class MainActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
-        try {
 
-            val myDatabase = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null)
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT)")
-
-            myDatabase.execSQL("INSERT INTO musicians(name, age) VALUES ('Okan',27) ")
-
-            val cursor = myDatabase.rawQuery("SELECT * FROM musicians", null)
-
-            val nameIx = cursor.getColumnIndex("name")
-            val ageIx = cursor.getColumnIndex("age")
-
-            while (cursor.moveToNext()) {
-                println("Name: " + cursor.getString(nameIx))
-                println("Age: " + cursor.getInt(ageIx))
-            }
-
-            cursor.close()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        try {
-
-            val database = this.openOrCreateDatabase("Person", MODE_PRIVATE,null)
-            database.execSQL("CREATE TABLE IF NOT EXISTS person (name VARCHAR,age INT)")
-            database.execSQL("INSERT INTO okan(name,age) VALUES ('okan',27)")
-
-            val cursor = database.rawQuery()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
 
     }
 }
