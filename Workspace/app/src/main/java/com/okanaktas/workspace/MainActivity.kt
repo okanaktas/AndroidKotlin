c3f2452f9dee04a843d02efe9b32d67fd506cee4
Okan Aktaş <okanaktas02@gmail.com>
2023-11-09 23:31:32 +0300

    SQLite

--- Workspace/app/src/main/java/com/okanaktas/workspace/MainActivity.kt
+++ Workspace/app/src/main/java/com/okanaktas/workspace/MainActivity.kt
@@ -16,6 +16,28 @@ class MainActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
+        try {
+
+            val database = this.openOrCreateDatabase("Person", MODE_PRIVATE, null)
+            database.execSQL("CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY, name VARCHAR, age INT)")
+            database.execSQL("INSERT INTO person (name, age) VALUES ('Okan',27) ")
+
+            val cursor = database.rawQuery("SELECT * FROM person", null)
+
+            val idIx = cursor.getColumnIndex("id")
+            val nameIx = cursor.getColumnIndex("name")
+            val ageIx = cursor.getColumnIndex("age")
+
+            while (cursor.moveToNext()) {
+                println("Id : ${cursor.getInt(idIx)}")
+                println("Name: ${cursor.getString(nameIx)}")
+                println("Age: ${cursor.getInt(ageIx)}")
+            }
+            cursor.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
 
     }
