a963c5db632c8597c913ec74bad93b44ff3e597d
Okan Aktaş <okanaktas02@gmail.com>
2024-02-09 16:22:01 +0300

    repo

--- Repo/app/src/main/java/com/okanaktas/repo/MapsActivity.kt
+++ Repo/app/src/main/java/com/okanaktas/repo/MapsActivity.kt
@@ -1,5 +1,8 @@
 package com.okanaktas.repo
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -15,6 +18,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +38,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
         val antalya = LatLng(36.904281824802915, 30.642897092382228)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antalya,15f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antalya, 15f))
         mMap.addMarker(MarkerOptions().position(antalya).title("Marker in Antalya"))
 
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
 
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
 
+            }
+        }
 
-
-
-
-
-
-
-
-
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        
 
     }
 }
