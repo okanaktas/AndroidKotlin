782d96257024bbd4cc47b57156d6a83607373c41
Okan Aktaş <okanaktas02@gmail.com>
2024-02-09 16:39:00 +0300

    Repo

--- Repo/app/src/main/java/com/okanaktas/repo/MapsActivity.kt
+++ Repo/app/src/main/java/com/okanaktas/repo/MapsActivity.kt
@@ -1,10 +1,17 @@
 package com.okanaktas.repo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,6 +19,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.okanaktas.repo.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -20,6 +28,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
+    private lateinit var permissionsLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +40,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        registerLauncher()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-
-        val antalya = LatLng(36.904281824802915, 30.642897092382228)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(antalya, 15f))
-        mMap.addMarker(MarkerOptions().position(antalya).title("Marker in Antalya"))
 
         locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
 
@@ -49,8 +56,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
-        
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Snackbar.make(binding.root, "Permissions needed for location", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                    //request permissions
+                    permissionsLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+            } else {
+                //request permission
+                permissionsLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        } else {
+            //permissions granted
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+        }
+
 
     }
+
+    private fun registerLauncher() {
+        permissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+
+            if (result) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+                }
+            } else {
+                Toast.makeText(this@MapsActivity, "Permissions needed!", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
+
 }
