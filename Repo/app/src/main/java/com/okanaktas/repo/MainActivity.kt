92ff76bcbf571d895edd34db4f036cf0125f8b60
Okan Aktaş <okanaktas02@gmail.com>
2024-02-21 23:53:49 +0300

    Create Repo App

--- Repo/app/src/main/java/com/okanaktas/repo/MainActivity.kt
+++ Repo/app/src/main/java/com/okanaktas/repo/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.okanaktas.repo
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun buttonGo(view : View){
-        var intent = Intent(this@MainActivity, MapsActivity::class.java)
-        startActivity(intent)
     }
 }
