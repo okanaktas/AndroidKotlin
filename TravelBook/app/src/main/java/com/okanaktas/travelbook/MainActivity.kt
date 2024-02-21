e12d8097204e2077a25414fd6b42be753fe77ed9
Okan Aktaş <okanaktas02@gmail.com>
2024-02-21 19:20:55 +0300

    Add Maps Activity

--- TravelBook/app/src/main/java/com/okanaktas/travelbook/MainActivity.kt
+++ TravelBook/app/src/main/java/com/okanaktas/travelbook/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.okanaktas.travelbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.okanaktas.travelbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+
+    fun buttonGo(view : View){
+        val intent = Intent(this@MainActivity,MapsActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ TravelBook/app/src/main/java/com/okanaktas/travelbook/MapsActivity.kt
@@ -0,0 +1,40 @@
+package com.okanaktas.travelbook
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.okanaktas.travelbook.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        
+
+    }
+}
