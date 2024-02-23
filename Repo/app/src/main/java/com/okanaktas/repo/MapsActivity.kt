83d78dd4b01b6a243c00e1ef0a7e14c58dacb396
Okan Aktaş <okanaktas02@gmail.com>
2024-02-23 14:30:52 +0300

    Update Repo App

---
+++ Repo/app/src/main/java/com/okanaktas/repo/MapsActivity.kt
@@ -0,0 +1,95 @@
+package com.okanaktas.repo
+
+import android.Manifest
+import android.app.LocaleManager
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import com.okanaktas.repo.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean : Boolean? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        registerLauncher()
+
+        sharedPreferences = this.getSharedPreferences("com.okanaktas.repo", MODE_PRIVATE)
+        trackBoolean = false
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+
+                val userLocation = LatLng(location.latitude,location.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,16f))
+
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Snackbar.make(binding.root, "Permission needed", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+            } else {
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        } else {
+            //permission granted
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+            mMap.isMyLocationEnabled = true
+        }
+
+    }
+
+    private fun registerLauncher() {
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (ContextCompat.checkSelfPermission(this@MapsActivity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+                mMap.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(this@MapsActivity, "Permission needed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
