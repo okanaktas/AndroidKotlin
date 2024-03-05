0d9f515926ef826845d9e53dfb09f27b4051067b
Okan Aktaş <okanaktas02@gmail.com>
2024-03-05 10:51:53 +0300

    Fragment Structure

--- FragmentKotlin/app/src/main/java/com/okanaktas/fragmentkotlin/MainActivity.kt
+++ FragmentKotlin/app/src/main/java/com/okanaktas/fragmentkotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.okanaktas.fragmentkotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+
+    fun button1(view: View) {
+        //fragment ı kullanabilmek için fragment yönetici diye bir yapıyı çağırmamız gerekiyor
+        val fragmentManager = supportFragmentManager
+        //yapıcağımız işlemleri başlatmak için fragmentTransaction diye bir yapı oluşturuyoruz. bu fragmentlarla bazı değişiklikler yapacağım ben diyoruz.
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val firstFragment = FirstFragment()
+        //İlk containerView istiyor. Kim barındıracak kim gösterecek bunu frameLayout umuz. İkincisi hangi fragment i gösterecek onu soruyor.
+        fragmentTransaction.replace(R.id.frameLayout,firstFragment).commit()
+    }
+
+    fun button2(view: View) {
+
+        //fragment ı kullanabilmek için fragment yönetici diye bir yapıyı çağırmamız gerekiyor
+        val fragmentManager = supportFragmentManager
+        //yapıcağımız işlemleri başlatmak için fragmentTransaction diye bir yapı oluşturuyoruz. bu fragmentlarla bazı değişiklikler yapacağım ben diyoruz.
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val secondFragment = SecondFragment()
+        //İlk containerView istiyor. Kim barındıracak kim gösterecek bunu frameLayout umuz. İkincisi hangi fragment i gösterecek onu soruyor.
+        fragmentTransaction.replace(R.id.frameLayout,secondFragment).commit()
     }
 }
---
+++ FragmentKotlin/app/src/main/java/com/okanaktas/fragmentkotlin/SecondFragment.kt
@@ -0,0 +1,24 @@
+package com.okanaktas.fragmentkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        return view
+    }
+
+}
