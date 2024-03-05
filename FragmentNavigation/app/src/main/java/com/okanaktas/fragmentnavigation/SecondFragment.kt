30c0c1add983d245fce51264aa54f5127ec7df17
Okan Aktaş <okanaktas02@gmail.com>
2024-03-05 13:26:34 +0300

    Fragment Navigation

---
+++ FragmentNavigation/app/src/main/java/com/okanaktas/fragmentnavigation/SecondFragment.kt
@@ -0,0 +1,39 @@
+package com.okanaktas.fragmentnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.okanaktas.fragmentnavigation.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    //onViewCreated oluşturup yazıyorum ki her şey oluşturulduktan sonra yazayım hata oluşmasın
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button2.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+
+    }
+
+}
