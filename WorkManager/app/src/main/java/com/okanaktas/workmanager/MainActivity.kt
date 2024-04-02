743c35db363bc8322c404948017343870a0fcce1
Okan Aktaş <okanaktas02@gmail.com>
2024-04-02 22:30:38 +0300

    Feature
    Work Manager, Periodic

--- WorkManager/app/src/main/java/com/okanaktas/workmanager/MainActivity.kt
+++ WorkManager/app/src/main/java/com/okanaktas/workmanager/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import java.util.concurrent.TimeUnit
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         val constraints = Constraints.Builder().setRequiresCharging(false).build()
 
+        /*
         val myWorkRequest : WorkRequest = OneTimeWorkRequestBuilder<RefreshDatabase>()
             .setConstraints(constraints)//build yapmadana önce istediğim constrainskeri ekledim
             .setInputData(data)//benim verim var mı ? var. Bunları gönder birlikte
@@ -35,6 +38,17 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         //calıştırmak için workmanagerın kendisini çağırıyorum
+        WorkManager.getInstance(this).enqueue(myWorkRequest)
+
+         */
+
+        //periodic oldugu zaman constructer vermemiz gerek
+        //Android en az 15 dakika ara ile bunu yapılabilmesine izin veriyor daha az olmuyor
+        val myWorkRequest : PeriodicWorkRequest = PeriodicWorkRequestBuilder<RefreshDatabase>(15,TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .setInputData(data)
+            .build()
+
         WorkManager.getInstance(this).enqueue(myWorkRequest)
     }
 }
